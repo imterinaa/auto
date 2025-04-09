ceb9315ccc22a47e3a6723e1f544869f22b64ec2
imterinaa <84046726+imterinaa@users.noreply.github.com>
2025-04-10 01:57:48 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,28 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import pytest
+import tempfile
+import os
+
 @pytest.fixture
 def browser():
+    user_data_dir = tempfile.mkdtemp()
+    
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    
     yield driver
     driver.quit()
+    try:
+        os.rmdir(user_data_dir)
+    except:
+        pass
